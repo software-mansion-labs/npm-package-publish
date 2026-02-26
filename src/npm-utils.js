df65a8e7658e9b2d1492bac403c26cfd831174b8
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-02-26 10:52:46 +0100

    Pipe npm errors and rethrow them

--- src/npm-utils.js
+++ src/npm-utils.js
@@ -7,10 +7,10 @@ function getPackageVersionByTag(packageName, tag) {
       : `npm view ${packageName} version`;
 
   try {
-    const result = execSync(npmString, { stdio: ['ignore', 'pipe', 'ignore'], timeout: 60000 }).toString().trim();
+    const result = execSync(npmString, { stdio: ['ignore', 'pipe', 'pipe'], timeout: 60000 }).toString().trim();
     return result;
   } catch (error) {
-    throw new Error(`Failed to get package version for ${packageName} by tag: ${tag}`);
+    throw new Error(`Failed to get package version for ${packageName} by tag: ${tag}`, { cause: error });
   }
 }
 
