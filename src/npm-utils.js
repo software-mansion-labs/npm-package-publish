f821c58b1b7c0ccc5c493354649803ea8c62555c
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-02-26 10:15:28 +0100

    Add 60s timeout

--- src/npm-utils.js
+++ src/npm-utils.js
@@ -7,7 +7,7 @@ function getPackageVersionByTag(packageName, tag) {
       : `npm view ${packageName} version`;
 
   try {
-    const result = execSync(npmString, { stdio: ['ignore', 'pipe', 'ignore'] }).toString().trim();
+    const result = execSync(npmString, { stdio: ['ignore', 'pipe', 'ignore'], timeout: 60000 }).toString().trim();
     return result;
   } catch (error) {
     throw new Error(`Failed to get package version for ${packageName} by tag: ${tag}`);
